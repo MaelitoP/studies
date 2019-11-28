b87f9e4bb3f978e18740924b832a45eea646ef3d
Maël LE PETIT <makibeardy@gmail.com>
2019-11-28 11:33:25 -0500

    Update tp1.js

--- tp1.js
+++ tp1.js
@@ -1,122 +1,100 @@
 /*
 * TP 1 - IFT1015
 * Auteurs:
-*   Mael LE PETIT (matricule),
-*   PERSONNE 2 (matricule)
+*   Mael Le Petit (20143452),
+*   Fannie Filion Bienvenue (20104125)
 *
-* // TODO : Description du programme
- */
+* Ce programme permet de modifier une image selon differents paramètres :
+* la mettre en noir et blanc, augmenté ou réduire la clarté, appliquer un
+* filtre "flou" et détecter les contours
+*/
 
-let canvas = document.getElementById('canvas');
-
-var grayScale = function(r, g, b)
-{
+//Fonction pour la formule de la luminance
+var nuanceGris = function(r, g, b) {
     return  (0.2126 * r) + (0.7152 * g) + (0.0722 *  b);
 };
 
-function noirEtBlanc(imageOriginale)
-{
-    for(var y = 0; y < canvas.height; y++)
-    {
-        for(var x = 0; x < canvas.width; x++)
-        {
-            var value = grayScale(imageOriginale[y][x].r, imageOriginale[y][x].g ,imageOriginale[y][x].b);
+//Fonction qui met l'image en noir et blanc
+function noirEtBlanc(imageOriginale) {
+    let height = imageOriginale.length,
+        width = imageOriginale[0].length;
 
-            imageOriginale[y][x].r = value; // red
-            imageOriginale[y][x].g = value; // green
-            imageOriginale[y][x].b = value; // blue
+    for (var y = 0; y < height; y++) {	//Iterer tous les pixels
+        for (var x = 0; x < width; x++) {
+            //Application de la fonction nuanceGris sur chacun des pixels
+            var valeur = nuanceGris(imageOriginale[y][x].r, imageOriginale[y][x].g ,imageOriginale[y][x].b);
+            imageOriginale[y][x].r = valeur;
+            imageOriginale[y][x].g = valeur;
+            imageOriginale[y][x].b = valeur;
         }
     }
     return imageOriginale;
 }
 
-/*
-*  Function 'calculateBrightness'
-*  c: Color value (red, green or blue)
-*  q: Brightness value (< 1: darker, > 1: brighter)
-*
-*  return the value for a pixel
- */
-function calculateBrightness(c, q)
-{
-    return (Math.pow((c/255), q) * 255);
+//Fonction qui calcul l'eclat selon la couleur du pixel. Valeur de l'eclat : < 1 = foncé, > 1 = claire
+function brightness(couleur, eclat) {
+    return (Math.pow((couleur/255), eclat) * 255);
 }
 
-function correctionClarte(imageOriginale, quantite)
-{
-    for(var y = 0; y < canvas.height; y++)
-    {
-        for(var x = 0; x < canvas.width; x++)
-        {
-            imageOriginale[y][x].r = calculateBrightness(imageOriginale[y][x].r, quantite); // red
-            imageOriginale[y][x].g = calculateBrightness(imageOriginale[y][x].g, quantite); // green
-            imageOriginale[y][x].b = calculateBrightness(imageOriginale[y][x].b, quantite); // blue
+//Fonction qui augmente ou diminue la clarté de l'image
+function correctionClarte(imageOriginale, quantite) {
+    let height = imageOriginale.length,
+        width = imageOriginale[0].length;
+
+    for (var y = 0; y < height; y++) {	//Iterer tous les pixels
+        for (var x = 0; x < width; x++) {
+            //Application de la fonction brightness sur chacun des pixels
+            imageOriginale[y][x].r = brightness(imageOriginale[y][x].r, quantite);
+            imageOriginale[y][x].g = brightness(imageOriginale[y][x].g, quantite);
+            imageOriginale[y][x].b = brightness(imageOriginale[y][x].b, quantite);
         }
     }
     return imageOriginale;
 }
 
-function hypotenuse(x1, y1, x2, y2)
-{
-    var xSquare = Math.pow(x1 - x2, 2);
-    var ySquare = Math.pow(y1 - y2, 2);
-    return Math.sqrt(xSquare + ySquare);
-}
+// Fonction qui applique un filtre "flou" a l'image
+function flou (imageOriginale, taille) {
+    let height = imageOriginale.length,
+        width = imageOriginale[0].length;
+    let imageRetourne = imageOriginale;
+    let ponderation = (1/Math.pow(taille, 2)); // Valeur de la ponderation selon la taille
 
-function matrice (row, column)
-{
-    let result = Array(row);
-    for (let i = 0; i < row; i++)
-        result[i] = Array(column);
+    let centre = Math.floor(taille/2); //Centre de la matrice
 
-    return result;
-}
+    for (let y = 0; y < height; y++) { // Iterer tous les pixels
+        for (let x = 0; x < width; x++) {
+            let red = ponderation * imageOriginale[y][x].r,
+                green = ponderation * imageOriginale[y][x].g,
+                blue = ponderation * imageOriginale[y][x].b;
 
-function flou (imageOriginale, taille)
-{
-    // Create (taille x taille) matrix & set each value at 1/N (N= taille pow 2)
-    let v = (1/Math.pow(taille, 2));
+            let indexX = x - centre,
+                indexY = y - centre,
+                verticale = 0; // y of matrix
 
-    var center = Math.floor(taille/2); // m center position
-
-    for (var y = 0; y < canvas.height; y++)
-    {
-        for (var x = 0; x < canvas.width; x++)
-        {
-            var red = v * imageOriginale[y][x].r;
-            var green = v * imageOriginale[y][x].g;
-            var blue = v * imageOriginale[y][x].b;
-
-            let indexX = x - center,
-                indexY = y - center,
-                w = 0, // x of matrix
-                z = 0; // y of matrix
-
-            for(var a = indexY; a < (indexY + taille); a++)
-            {
-                for(var b = indexX; b < (indexX + taille); b++)
-                {
-                    if (b > 0 && b < canvas.width && a > 0 && a < canvas.height) // Out of bounds
-                    {
-                        red += v * imageOriginale[a][b].r;
-                        green += v * imageOriginale[a][b].g;
-                        blue += v * imageOriginale[a][b].b;
+            for(let a = indexY; a < (indexY + taille); a++) {
+                let horizontale = 0; // x of matrix
+                for(let b = indexX; b < (indexX + taille); b++) {
+                    if (b > 0 && b < width && a > 0 && a < height) { // Depassement des bornes
+                        red += ponderation * imageOriginale[a][b].r;
+                        green += ponderation * imageOriginale[a][b].g;
+                        blue += ponderation * imageOriginale[a][b].b;
                     }
-                    w++;
+                    horizontale++;
                 }
-                z++;
+                verticale++;
             }
-            imageOriginale[y][x].r = red;
-            imageOriginale[y][x].g = green;
-            imageOriginale[y][x].b = blue;
+            imageRetourne[y][x].r = red;
+            imageRetourne[y][x].g = green;
+            imageRetourne[y][x].b = blue;
         }
     }
-
-    return imageOriginale;
+    return imageRetourne;
 }
 
 // Fonction qui dessine les contours
 function detectionContours(imageOriginale) {
+    let height = imageOriginale.length,
+        width = imageOriginale[0].length;
     // Mettre l'image en noir et blanc afin d'avoir la meme valeur pour les pixels r, g, b
     var image = noirEtBlanc(imageOriginale),
         imageRetourne = imageOriginale;
@@ -125,8 +103,8 @@ function detectionContours(imageOriginale) {
     let ponderation_verticale = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]],
         ponderation_horizontale = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]];
 
-    for (var y = 0; y < canvas.height; y++) {	// Iterer tous les pixels
-        for (var x = 0; x < canvas.width; x++) {
+    for (var y = 0; y < height; y++) {	// Iterer tous les pixels
+        for (var x = 0; x < width; x++) {
             var variationX = 0, variationY = 0,
                 bound = 3;
 
@@ -134,7 +112,7 @@ function detectionContours(imageOriginale) {
             for(var a = y; a < (y + bound); a++) {	// Iterer les pixels de la matrice 3 x 3
                 var horizontale = 0;
                 for(var b = x; b < (x + bound); b++) {
-                    if (a >= 0 && a < canvas.height && b >= 0 && b < canvas.width) {
+                    if (a >= 0 && a < height && b >= 0 && b < width) {
                         var couleur = image[a][b].r; // Choix du canal rouge
                         variationX += (ponderation_horizontale[verticale][horizontale] * couleur);
                         variationY += (ponderation_verticale[verticale][horizontale] * couleur);
@@ -157,8 +135,101 @@ function detectionContours(imageOriginale) {
     return imageRetourne;
 }
 
-function tests()
-{
-    /* TODO : Ajoutez des tests unitaires pour les 4 fonctions
-       demandées et pour vos propres fonctions */
+
+function hex_to_rgb (h) {
+    let red = 0, green = 0, blue = 0;
+
+    // 3 digits
+    if (h.length === 4) {
+        red = "0x" + h[1] + h[1];
+        green = "0x" + h[2] + h[2];
+        blue = "0x" + h[3] + h[3];
+
+        // 6 digits
+    } else if (h.length === 7) {
+        red = "0x" + h[1] + h[2];
+        green = "0x" + h[3] + h[4];
+        blue = "0x" + h[5] + h[6];
+    }
+    return { r: +red, g: +green, b: +blue  };
 }
+
+function gradient(couleurA, couleurB) {
+    var gradient = [];
+    var maxValeur = 255;
+    var de = hex_to_rgb(couleurA+"");
+    var vers = hex_to_rgb(couleurB+"");
+
+    for (var i = 0; i <= maxValeur; i++) {
+        var intensiteB = i;
+        var intensiteA = maxValeur - intensiteB;
+
+        gradient[i] = {
+            r: (intensiteA*vers.r + intensiteB*de.r) / maxValeur,
+            g: (intensiteA*vers.g + intensiteB*de.g) / maxValeur,
+            b: (intensiteA*vers.b + intensiteB*de.b) / maxValeur
+        };
+    }
+    return gradient;
+}
+
+function rotationCouleurs (imageOriginale, couleurOriginal, couleur) {
+    let height = imageOriginale.length,
+        width = imageOriginale[0].length;
+
+    var couleur_gradient = gradient(couleurOriginal, couleur);
+
+    noirEtBlanc(imageOriginale);
+
+    for (var y = 0; y < height; y++) {	//Iterer tous les pixels
+        for (var x = 0; x < width; x++) {
+            var redValue = imageOriginale[y][x].r;
+            var greenValue = imageOriginale[y][x].g;
+            var blueValue = imageOriginale[y][x].b;
+
+
+            console.log(redValue);
+            console.log(couleur_gradient[redValue]);
+            imageOriginale[y][x].r = couleur_gradient[redValue].r;
+            imageOriginale[y][x].g = couleur_gradient[greenValue].g;
+            imageOriginale[y][x].b = couleur_gradient[blueValue].b;
+        }
+    }
+    return imageOriginale;
+}
+
+function tests() {
+    var imageTest  = [[{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 45, g: 35, b: 25}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}]];
+
+    // Check pour la fonction 'noirEtBlanc'
+    console.assert(JSON.stringify(noirEtBlanc(imageTest)) === JSON.stringify([
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 36.404, g: 36.404, b: 36.404}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}]]));
+
+    // Check pour la fonction 'correctionClarte'
+    console.assert(JSON.stringify(correctionClarte(imageTest, 1.5)) === JSON.stringify([
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: Math.pow(3/17, 1.5) * 255,
+            g: Math.pow(7/51, 1.5) * 255, b: Math.pow(5/51, 1.5) * 255}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}]]));
+
+    // Check pour la fonction 'flou'
+    console.assert(JSON.stringify(flou(imageTest, 3)) === JSON.stringify([
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 5, g: 35/9, b: 25/9}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}]]));
+
+    // Check pour la fonction 'detectionContours'
+    console.assert(JSON.stringify(detectionContours(imageTest)) === JSON.stringify([
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 36, g: 36, b: 36}, {r: 0, g: 0, b: 0}],
+        [{r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}, {r: 0, g: 0, b: 0}]]));
+
+    console.assert(hex_to_rgb("#00FFFF") ===  { r: 0, g: 255, b: 255  });
+    console.assert(brightness(35, 0.5) === (Math.pow((35/255), 0.5) * 255))
+}
+
+tests();
