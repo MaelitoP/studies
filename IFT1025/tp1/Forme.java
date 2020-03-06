113e373a7acde8e72a28fee490e65feeb57bca47
Maël LE PETIT <makibeardy@gmail.com>
2020-03-06 13:46:00 -0500

    Update Forme.java

--- IFT1025/tp1/Forme.java
+++ IFT1025/tp1/Forme.java
@@ -1,32 +1,35 @@
+package fr.shape;
+
 import fr.Tp1;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Forme
+abstract class Forme
 {
-    private int x0, y0, xLength, yLength;
+    private int x0, y0, x1, y1, xLength, yLength;
     private char[][] shape;
     private char c;
     private int diff;
 
     public static List<Forme> list = new ArrayList<Forme>();
 
-    public Forme(int x0, int y0, int xLength, int yLength)
+    public Forme(int x0, int y0, int x1, int y1)
     {
         this.x0 = x0;
         this.y0 = y0;
-        this.xLength = xLength;
-        this.yLength = yLength;
+        this.x1 =  x1;
+        this.y1  = y1;
+
+        this.xLength = (x1-x0+1) > 0 ? (x1-x0+1) : -(x1-x0-1);
+        this.yLength = (y1-y0+1) > 0 ? (y1-y0+1) : -(y1-y0-1);
+
         this.shape = new char[yLength][xLength];
         this.c = '#';
         this.diff = 0;
     }
 
-    public void init()
-    {
-
-    }
+    abstract void init();
 
     public void setDiff(int i)
     {
@@ -66,6 +69,16 @@ public class Forme
     public int getY0()
     {
         return this.y0;
+    }
+
+    public int getX1()
+    {
+        return this.x1;
+    }
+
+    public int getY1()
+    {
+        return this.y1;
     }
 
     public void setX0(int x0)
